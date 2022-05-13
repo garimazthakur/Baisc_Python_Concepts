dbd97513d2f59a64dd35037281b377655493229c
Garima Thakur <garima.thakur6@gmail.com>
2022-05-13 12:39:41 +0530

    commit

--- List/Iteration/run.py
+++ List/Iteration/run.py
@@ -1,27 +1,46 @@
 list1= [(2,3), (4,5), (6,7)]
-print(list1[1][0])
-for x in list1:
-    print(x[0])
-        # print(y[1])
-# for item, val in enumerate(t):
-#     for val in item:
-#         print(val)
-# print("*********************************************")
-# list2= [5, 2,3, 4,5, 6,7]
-# # print(list2[0])
-# for idx, value in enumerate(list2):
-#     pair = idx, value
-   
-#     _, pair1= idx, value
-    
-#     pair2, _ = idx, value 
-    
-# print(pair)
-# print(pair1)
-# print(pair2)
-    
 
+k, v  = zip(*list1)
+print(k)  #(2, 4, 6)
+print(v) #(3, 5, 7)
+# ********************************
+print(list1[1][0])  #4
+for x in list1:
+    print(x[0]) 
+    #output: 
+    # 1
+    # 2
+    # 3  
     
+for inde, val in enumerate(list1):
+    print(val)
+    # output
+    # (2, 3)
+    # (4, 5)
+    # (6, 7)
+
+
+print("*********************************************")
+
+list2= [5, 2,3, 4,5, 6,7]
+lis= []
+lis1=[]
+lis2=[]
+for idx, value in enumerate(list2):
+    
+    pair = idx, value
+    lis.append(pair)
+    
+    _, pair1= idx, value
+    lis1.append(pair1)
+    
+    pair2, _ = idx, value 
+    lis2.append(pair2)
+
+print(lis) # [(0, 5), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7)]
+print(lis1) #[5, 2, 3, 4, 5, 6, 7]
+print(lis2) #[0, 1, 2, 3, 4, 5, 6]
+
    
     
     
