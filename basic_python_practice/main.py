44b795e979044f5e6a109dc320b87090041b6744
Garima Thakur <garima.thakur6@gmail.com>
2022-05-26 18:02:23 +0530

    commit

---
+++ basic_python_practice/main.py
@@ -0,0 +1,13 @@
+import numpy as np
+nums=[3,1,2,1,5,8]
+print(sorted(nums))     #output: [1, 1, 2, 3, 5, 8]
+
+nums1=[3,1,2,1,5,8]
+print(reversed(nums1))   #output: <list_reverseiterator object at 0x7f37803dcd30>
+
+
+
+
+
+
+ 
